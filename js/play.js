beedd3468e29b49e05c9fa78522bcdc0aad04356
Matt Dienna <matt.dienna@gmail.com>
2022-08-27 19:04:52 -0400

    Added code to allow first card to be dropped only in the specified target space

--- js/play.js
+++ js/play.js
@@ -1,87 +1,80 @@
-
-  document.addEventListener("contextmenu", function(event) {
-    event.preventDefault();
-    alert(event.target.src);
-  })
-  
-  document.addEventListener("dragstart", function(event) {
-  // The dataTransfer.setData() method sets the data type and the value of the dragged data
+    //Code for  game play
     
-
-    event.dataTransfer.setData("text", event.target.id);
-    if (event.target.className === "card p1") {
-        event.target.style.border = "3px solid rgb(6, 39, 54)";
-        let hiLite = event.target.style.border;
-
-    } else
-        event.target.style.border = "3px solid rgb(177, 151, 4)";
-        let hiLite = event.target.style.border;
+    let firstCard = 1;
+   
+    // The dataTransfer.setData() method sets the data type and the value of the dragged data
     
+    document.addEventListener("dragstart", function(event) {
+        event.dataTransfer.setData("text", event.target.id);
+        event.target.style.border = "4px solid gold";
     });
 
-// When entering a target area change border to dotted red
-       document.addEventListener("dragenter", function(event) {
-       // event.target.style.opacity = 1;
-        if ( event.target.className == "droptarget" ) {
-         event.target.style.border = "2px dotted red";
-  }
-});
+    // While dragging change the border color
+    document.addEventListener("drag", function(event) {
+        event.target.style.border = "4px dashed gold";
+        // Get element by ID that contains`card`
+        const cardID = document.querySelector(`[id*="card"]`).id;   
+     });
 
-document.addEventListener("dragenter", function(event) {
-  // event.target.style.opacity = 1;
-        if ( event.target.className == "droptargetC" ) {
-        event.target.style.border = "2px dotted red";
-  }
-});
+    // At dragend change the border color
+    document.addEventListener("dragend", function(event) {
+        event.target.style.border = "";
+    });
 
-// To allow a drop, we must prevent the default handling of the element
-        document.addEventListener("dragover", function(event) {
-        event.preventDefault();
-});
+    //On entering the drop area, check for both target areas
+    document.addEventListener("dragenter", function(event) {
+        if (event.target.className == "droptargetC" ) {
+            event.target.style.border = "4px dotted red";
+        } 
+        else if (event.target.className == "droptarget") {
+            event.target.style.border = " 4px dotted white";
+        }
+    });
 
-// When the draggable element leaves the droptarget, reset the border style
-         document.addEventListener("dragleave", function(event) {
-          if ( event.target.className == "droptarget" ) {
-           event.target.style.border = "";
-  }
-});
+    // To allow a drop, we must prevent the default handling of the element
+    document.addEventListener("dragover", function(event) {
+            event.preventDefault(); 
+    });
 
-          document.addEventListener("dragleave", function(event) {
-          if ( event.target.className == "droptargetC" ) {
-          event.target.style.border = "";
-  } 
-});
+    // When the draggable element leaves the droptarget, reset the border style
+    document.addEventListener("dragleave", function(event) {
+        if (event.target.className == "droptargetC" ) {
+            event.target.style.border = "";
+        } 
+        else if (event.target.className == "droptarget") {
+            event.target.style.border = "";
+        }
+    });
+  
+      // On drop append the dragged element into the drop target
+ 
+      document.addEventListener("drop", function(event) {
+            
+        if (firstCard == 1 && event.target.className == "droptargetC") {
+            event.preventDefault();
+            event.target.style.border = "";
+            const data = event.dataTransfer.getData("text");
+            event.target.appendChild(document.getElementById(data));
+            firstCard ++;
+            
+        } else {
 
-/* On drop append the dragged element into the drop target
-*/
-    document.addEventListener("drop", function(event) {
-    event.preventDefault();
-        if ( event.target.className == "droptarget" ) {
-           event.target.style.border = "";
-           //event.target.style.border = hiLite;
-           const data = event.dataTransfer.getData("text");
-           event.target.appendChild(document.getElementById(data));
-  }
-});
-
-    document.addEventListener("drop", function(event) {
-    event.preventDefault();
-      if ( event.target.className == "droptargetC" ) {
-         event.target.style.border = "";
-         //event.target.style.border = hiLite;
-         const data = event.dataTransfer.getData("text");
-         event.target.appendChild(document.getElementById(data));
-}
-});
-
-
-
+        if (firstCard > 1 && event.target.className == "droptarget") {
+            event.preventDefault();
+            event.target.style.border = "";
+            const data = event.dataTransfer.getData("text");
+            event.target.appendChild(document.getElementById(data));
+            }
+        }
+    });
+    
+  
 //jQuery code to rotate image 90deg with each click event
 
 function rotateCard(x) {
-  let angle = ($('#card'+x).data('angle') + 90) || 90;
-  $('#card'+x).css({'transform': 'rotate(' + angle + 'deg)'});
-  $('#card'+x).data('angle', angle);
-}
-
- 
+    let angle = ($('#card'+x).data('angle') + 90) || 90;
+    $('#card'+x).css({'transform': 'rotate(' + angle + 'deg)'});
+    $('#card'+x).data('angle', angle);
+  }
+  
+   
